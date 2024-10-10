dbeb099388f2ebcb2a91e7819d028b9724a9facc
YgorVSPaulino <ygor.paulino.1999@gmail.com>
2024-10-10 11:21:04 -0300

    subindo projeto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+/* import Nav from './components/Nav.vue'; */
+</script>
+
+<template>
+  <header class="p-4">
+    <div class="container mx-auto flex justify-between items-center">
+      <img src="./assets/imgs/logo.png" alt="" width="150px">
+      <nav>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterLink></RouterLink>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,10 @@
+<!-- <template>
+    <header class="p-4">
+    <div class="container mx-auto flex justify-between items-center">
+      <img src="../assets/imgs/Logo.png" alt="" width="150px">
+      <nav>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template> -->
