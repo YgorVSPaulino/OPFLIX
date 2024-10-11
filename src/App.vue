26ebb5941f3f33f92ae63f75af4df63cad9f7374
YgorVSPaulino <ygor.paulino.1999@gmail.com>
2024-10-11 10:32:16 -0300

    atualizando projetoo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
+import { RouterView } from 'vue-router';
 import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
   <HomeView></HomeView>
+  <RouterView></RouterView>
 </template>
 
 <style scoped>
-</style>
-
-<style scoped>
-
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,59 @@
+<script setup>
+import Nav from "../components/Nav.vue";
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <main class="bg-main">
+    <!-- Componente Nav incluído no HomeView -->
+    <Nav></Nav>
+    <!-- Conteúdo do HomeView -->
+    <div class="flex justify-center items-center py-36">
+    <div class="w-full max-w-md">
+      <div class="bg-black opacity-80 p-11 shadow-lg rounded-lg">
+        <h2 class="text-2xl font-bold text-center mb-6 text-white">Entrar</h2>
+
+        <form @submit.prevent="submitLogin">
+          <div class="mb-4">
+            <label for="email" class="block text-gray-700 font-bold mb-2">Email</label>
+            <input
+              v-model="email"
+              id="email"
+              type="email"
+              placeholder="Digite seu email"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+
+          <div class="mb-6">
+            <label for="password" class="block text-gray-700 font-bold mb-2">Senha</label>
+            <input
+              v-model="password"
+              id="password"
+              type="password"
+              placeholder="Digite sua senha"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+
+          <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+            Entrar
+          </button>
+
+          <a href="#" class="block text-center mt-4 text-sm text-blue-500 hover:text-blue-700">Esqueceu sua senha?</a>
+        </form>
+      </div>
+    </div>
   </div>
+  </main>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+.bg-main {
+  background-image: url("../assets/imgs/background2.png");
+  background-size: cover;
+  background-position: center;
+  height: 100vh; /* para cobrir a tela toda */
 }
 </style>
